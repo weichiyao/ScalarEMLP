ca6a4ce8c5d0eeb9c3f376837975e44d946f2526
Weichi Yao <wy635@stern.nyu.edu>
2022-01-23 19:56:52 -0600

    Update

--- experiments/hnn_scalars.py
+++ experiments/hnn_scalars.py
@@ -1,15 +1,15 @@
-from emlp.nn import InvarianceLayer_objax,InvarianceLayerDL_objax,compute_scalars,radial_basis_transform
+from emlp.nn import ScalarMLP, InvarianceLayer_objax, ScalarTransformer
 from trainer.hamiltonian_dynamics import IntegratedDynamicsTrainer,DoubleSpringPendulum,hnnScalars_trial
 from torch.utils.data import DataLoader
 from oil.utils.utils import cosLr,FixedNumpySeed,FixedPytorchSeed
 from trainer.utils import LoaderTo
 from oil.datasetup.datasets import split_dataset
-from oil.tuning.args import argupdated_config
-import torch.nn as nn
+from oil.tuning.args import argupdated_config 
 import logging
 import emlp
 import emlp.reps
 import objax
+import numpy as np
 
 
 levels = {'critical': logging.CRITICAL,'error': logging.ERROR,
@@ -18,27 +18,36 @@ levels = {'critical': logging.CRITICAL,'error': logging.ERROR,
 
 
 def makeTrainerScalars(*,dataset=DoubleSpringPendulum,num_epochs=2000,ndata=5000,seed=2021, 
-                       dimensionless=False,
-                       n_rad=200,bs=500,lr=5e-3,device='cuda',split={'train':500,'val':.1,'test':.1},
-                       data_config={'chunk_len':5,'dt':0.2,'integration_time':30,'regen':False},
-                       net_config={'n_layers':3,'n_hidden':100}, log_level='info',
-                       trainer_config={'log_dir':'/home/','log_args':{'minPeriod':.02,'timeFrac':.75},},
-                       save=False,):
+                bs=500,lr=5e-3,device='cuda',split={'train':-1,'val':500,'test':500},
+                data_config={'chunk_len':5,'dt':0.2,'integration_time':4,'regen':False},
+                transformer_config={
+                    'method':'qt', 'dimensionless':True, 'n_rad':50, 
+                    'n_quantiles':200, 'transform_distribution':'uniform'
+                },
+                net_config={'n_layers':3,'n_hidden':100}, log_level='info',
+                trainer_config={'log_dir':'/home/','log_args':{'minPeriod':.02,'timeFrac':.75},},
+                save=False,):
     logging.getLogger().setLevel(levels[log_level])
     # Prep the datasets splits, model, and dataloaders
     with FixedNumpySeed(seed),FixedPytorchSeed(seed):
-        base_ds = dataset(n_systems=ndata,**data_config)
+        base_ds = dataset(n_systems=ndata, **data_config)
         datasets = split_dataset(base_ds,splits=split)
     
-    if dimensionless:
-        model = InvarianceLayerDL_objax(**net_config)      
-    else:
-        model = InvarianceLayer_objax(**net_config)
-          
+        zs_train = base_ds.Zs[datasets['train']._ids].reshape(-1,4,3)
+        zps_train = np.repeat(
+            base_ds.ZPs[datasets['train']._ids], 
+            data_config['chunk_len'], 
+            axis=0
+        ) 
+        stransformer = ScalarTransformer(
+            zs = zs_train, 
+            zps = zps_train,   
+            **transformer_config
+        )
+    model = InvarianceLayer_objax(transformer=stransformer, **net_config)
     dataloaders = {k:LoaderTo(DataLoader(v,batch_size=min(bs,len(v)),shuffle=(k=='train'),
                    num_workers=0,pin_memory=False)) for k,v in datasets.items()}
     dataloaders['Train'] = dataloaders['train']
-    
     opt_constr = objax.optimizer.Adam
     # lr_sched = lambda e: lr#*cosLr(num_epochs)(e)#*min(1,e/(num_epochs/10))
     lr_sched = lambda e: lr if (e < 200) else (lr*0.4 if e < 1000 else (lr*0.1))   
@@ -46,6 +55,6 @@ def makeTrainerScalars(*,dataset=DoubleSpringPendulum,num_epochs=2000,ndata=5000
 
 if __name__ == "__main__":
     Trial = hnnScalars_trial(makeTrainerScalars)
-    cfg,outcome = Trial(argupdated_config(makeTrainerScalars.__kwdefaults__,namespace=(emlp.groups,emlp.nn)),i=1)
+    cfg,outcome = Trial(argupdated_config(makeTrainerScalars.__kwdefaults__,namespace=(emlp.groups,emlp.nn)))
     print(outcome)
 
