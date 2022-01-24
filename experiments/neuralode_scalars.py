6870e60d4262cf6fcdffa7ffabd025f789277879
Weichi Yao <wy635@stern.nyu.edu>
2022-01-23 19:57:32 -0600

    Update

--- experiments/neuralode_scalars.py
+++ experiments/neuralode_scalars.py
@@ -1,4 +1,4 @@
-from emlp.nn import EquivarianceLayer_objax,compute_scalars,radial_basis_transform
+from emlp.nn import EquivarianceLayer_objax, ScalarTransformer, ScalarMLP
 from trainer.hamiltonian_dynamics import IntegratedODETrainer,DoubleSpringPendulum,odeScalars_trial
 from torch.utils.data import DataLoader
 from oil.utils.utils import FixedNumpySeed,FixedPytorchSeed
@@ -9,6 +9,7 @@ import logging
 import emlp.nn
 import emlp.groups
 import objax
+import numpy as np
  
 
 
@@ -17,11 +18,15 @@ levels = {'critical': logging.CRITICAL,'error': logging.ERROR,
                     'info': logging.INFO,'debug': logging.DEBUG}
 
 def makeTrainerScalars(*,dataset=DoubleSpringPendulum,num_epochs=2000,ndata=5000,seed=2021,aug=False,
-                       n_rad=200,bs=500,lr=5e-3,device='cuda',split={'train':500,'val':.1,'test':.1},
-                       data_config={'chunk_len':5,'dt':0.2,'integration_time':30,'regen':False},
-                       net_config={'n_layers':3,'n_hidden':100},log_level='warn',
-                       trainer_config={'log_dir':None,'log_args':{'minPeriod':.02,'timeFrac':.75},}, 
-                       save=True,):
+                bs=500,lr=5e-3,device='cuda',split={'train':500,'val':.1,'test':.1},
+                data_config={'chunk_len':5,'dt':0.2,'integration_time':30,'regen':False},
+                transformer_config={
+                    'method':'rbf', 'dimensionless':False, 'n_rad':50, 
+                    'n_quantiles':1000, 'transform_distribution':'uniform'
+                },
+                net_config={'n_layers':3,'n_hidden':100},log_level='warn',
+                trainer_config={'log_dir':None,'log_args':{'minPeriod':.02,'timeFrac':.75},}, 
+                save=False,):
 
     logging.getLogger().setLevel(levels[log_level])
     # Prep the datasets splits, model, and dataloaders
@@ -29,16 +34,19 @@ def makeTrainerScalars(*,dataset=DoubleSpringPendulum,num_epochs=2000,ndata=5000
         base_ds = dataset(n_systems=ndata,**data_config)
         datasets = split_dataset(base_ds,splits=split)
     
-    z0_train = base_ds.Zs[datasets['train']._ids,0,:]
-    zps_train = base_ds.ZPs[datasets['train']._ids]
-    scalars_z0 = compute_scalars(z0_train.reshape(-1,4,3), zps_train)
-    trans_mu, trans_gamma = radial_basis_transform(scalars_z0, nrad = n_rad) 
-    model = EquivarianceLayer_objax(
-        n_layers=net_config['n_layers'], 
-        n_hidden=net_config['n_hidden'],
-        mu=trans_mu,
-        gamma=trans_gamma
-    )
+        zs_train = base_ds.Zs[datasets['train']._ids].reshape(-1,4,3)
+        zps_train = np.repeat(
+            base_ds.ZPs[datasets['train']._ids], 
+            data_config['chunk_len'], 
+            axis=0
+        ) 
+        stransformer = ScalarTransformer(
+            zs = zs_train, 
+            zps = zps_train,  
+            seed = seed, 
+            **transformer_config
+        )
+    model = EquivarianceLayer_objax(transformer=stransformer, **net_config)
     
     dataloaders = {k:LoaderTo(DataLoader(v,batch_size=min(bs,len(v)),shuffle=(k=='train'),
                    num_workers=0,pin_memory=False)) for k,v in datasets.items()}
