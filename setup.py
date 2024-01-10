2e7e2c51bda5d75c3ebf33491abe85f5a266f74e
Weichi Yao <wy2244@columbia.edu>
2024-01-10 09:58:29 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,22 +9,21 @@ def get_property(prop, project):
 
 project_name = "ScalarEMLP"
 setup(name=project_name,
-      description="Scalar-based Equivariant Machine Learning models",
+      description="A Practical Method for Constructing Equivariant Multilayer Perceptrons for Arbitrary Matrix Groups",
       version= get_property('__version__',project_name),
-      author='Weichi Yao',
-      author_email='weichiy@umich.edu',
+      author='Marc Finzi',
+      author_email='maf820@nyu.edu',
       license='MIT',
-      python_requires='>=3.8', 
+      python_requires='>=3.6',
       install_requires=['h5py','objax','pytest','plum-dispatch',
-                        'optax','tqdm>=4.38','matplotlib','scikit-learn',
-                        'pytorch-lightning','lightning'],
+      'optax','tqdm>=4.38','matplotlib','scikit-learn'],
       extras_require = {
           'EXPTS':['olive-oil-ml']
       },
       packages=find_packages(),
-      long_description=open('README.md').read(),
+      long_description=open('README.md', encoding='UTF-8').read(),
       long_description_content_type='text/markdown',
-      url='https://github.com/weichiyao/ScalarEMLP',
+      url='https://github.com/mfinzi/equivariant-MLP',
       classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
