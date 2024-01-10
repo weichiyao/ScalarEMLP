09f3780f11ce0889af0d4cc55adef02aac13d7b6
Weichi Yao <wy2244@columbia.edu>
2024-01-10 12:25:45 -0500

    Update setup.py
    packages dependence

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ def get_property(prop, project):
     result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/__init__.py').read())
     return result.group(1)
 
-project_name = "emlp"
+project_name = "ScalarEMLP"
 setup(name=project_name,
       description="A Practical Method for Constructing Equivariant Multilayer Perceptrons for Arbitrary Matrix Groups",
       version= get_property('__version__',project_name),
-      author='Marc Finzi',
-      author_email='maf820@nyu.edu',
+      author='Weichi Yao',
+      author_email='weichiy@umich.edu',
       license='MIT',
-      python_requires='>=3.6',
-      install_requires=['h5py','objax','pytest','plum-dispatch',
-            'olive-oil-ml','optax','tqdm>=4.38'],
+      python_requires='>=3.8',
+      install_requires=['h5py','objax','pytest','plum-dispatch','scikit-learn>=1.3.2',
+            'olive-oil-ml>=0.1.1','optax','tqdm>=4.38','pytorch-lightning','lightning'],
       packages=find_packages(),
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
